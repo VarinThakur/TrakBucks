3063cecfa29ce9a364077338c27fb65a9d039b99
Varin Thakur <varinthaur_co20b7_36@dtu.ac.in>
2022-06-14 14:27:16 +0530

    Use shared preferences to handle settings changes.

--- app/src/main/java/com/example/trakbucks/SettingsFragment.kt
+++ app/src/main/java/com/example/trakbucks/SettingsFragment.kt
@@ -1,70 +1,79 @@
 package com.example.trakbucks
 
+import android.content.Intent
 import android.content.SharedPreferences
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import android.widget.Toast.LENGTH_SHORT
+import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.ViewModelProvider
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import androidx.preference.PreferenceManager
+import com.example.trakbucks.data.TransactionApplication
 import com.example.trakbucks.data.TransactionViewModel
+import com.example.trakbucks.data.TransactionViewModelFactory
+import kotlin.properties.Delegates
 
 
 class SettingsFragment : PreferenceFragmentCompat(), OnSharedPreferenceChangeListener {
 
-    private lateinit var myTransactionViewModel: TransactionViewModel
+//    var myTransactionViewModel2: TransactionViewModel by Delegates.notNull<TransactionViewModel>()
+//    private val myTransactionViewModel: TransactionViewModel by activityViewModels {
+//        TransactionViewModelFactory(
+//            (requireActivity().application as TransactionApplication).database
+//                .transactionDao()
+//        )
+//    }
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
 
-        myTransactionViewModel= ViewModelProvider(this).get(TransactionViewModel::class.java)
-
         setPreferencesFromResource(R.xml.settings, rootKey)
-        PreferenceManager.getDefaultSharedPreferences(requireContext()).registerOnSharedPreferenceChangeListener(this)
+        PreferenceManager.getDefaultSharedPreferences(requireContext())
+            .registerOnSharedPreferenceChangeListener(this)
+
+        //myTransactionViewModel2 = assignViewModel()
 
         val examplePreference: Preference? = findPreference("signOut")
         examplePreference?.onPreferenceClickListener = Preference.OnPreferenceClickListener {
 
-            myTransactionViewModel= ViewModelProvider(this).get(TransactionViewModel::class.java)
-            myTransactionViewModel.deleteAllTransactions()
+            //myTransactionViewModel.deleteAllTransactions()
 
-            val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(requireContext() /* Activity context */)
-            with(sharedPreferences?.edit())
-            {
-                this?.putString("signOut","false")
-                this?.apply()
+            val sharedPreferences =
+                PreferenceManager.getDefaultSharedPreferences(requireContext() /* Activity context */)
+            reset(sharedPreferences)
+            val intent = Intent(requireContext(), MainActivity::class.java).apply {
             }
-            activity?.recreate()
+            startActivity(intent)
+            requireActivity().finish()
             return@OnPreferenceClickListener true
         }
     }
 
     override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences?, key: String?) {
-        if(key == "Theme")
-        {
-            val prefs = sharedPreferences?.getString(key,"Red")
-
-            if(prefs != myTransactionViewModel.theme.value) {
-                myTransactionViewModel.updateTheme(prefs!!)
-                Toast.makeText(activity, "Theme Applied!", LENGTH_SHORT).show()
-                activity?.recreate()
-            }
+        if (key == "Theme") {
+            val prefs = sharedPreferences?.getString(key, "Red")
+            applyTheme(sharedPreferences,prefs!!)
+            requireActivity().recreate()
         }
-        if(key == "Currency")
-        {
-            val prefs = sharedPreferences?.getString(key,"INR")
-            when(prefs)
-            {
-                "INR"-> {Toast.makeText(activity,"Set to Indian Rupee",LENGTH_SHORT).show()
+        if (key == "Currency") {
+            val prefs = sharedPreferences?.getString(key, "INR")
+            when (prefs) {
+                "INR" -> {
+                    Toast.makeText(activity, "Set to Indian Rupee", LENGTH_SHORT).show()
 
                 }
-                "DOL"-> {Toast.makeText(activity,"Set to American Dollar",LENGTH_SHORT).show()}
-                "EUR"-> {Toast.makeText(activity,"Set to Indian Rupee",LENGTH_SHORT).show()}
+                "DOL" -> {
+                    Toast.makeText(activity, "Set to American Dollar", LENGTH_SHORT).show()
+                }
+                "EUR" -> {
+                    Toast.makeText(activity, "Set to Indian Rupee", LENGTH_SHORT).show()
+                }
             }
         }
     }
-
 
 
     override fun onResume() {
@@ -76,4 +85,39 @@ class SettingsFragment : PreferenceFragmentCompat(), OnSharedPreferenceChangeLis
         super.onPause()
         preferenceManager.sharedPreferences?.unregisterOnSharedPreferenceChangeListener(this)
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("Frag", "Fragment Destroyed!")
+    }
+
+    private fun reset(sharedPreferences: SharedPreferences?){
+        with(sharedPreferences?.edit())
+        {
+            this?.putString("signOut", "false")
+            this?.putString("Currency","INR")
+            this?.putString("Theme","Red")
+            this?.apply()
+        }
+    }
+
+    private fun applyTheme(sharedPreferences: SharedPreferences?, theme : String ){
+        with(sharedPreferences?.edit())
+        {
+            this?.putString("Theme",theme)
+            this?.apply()
+        }
+    }
+
+//    private fun assignViewModel() : TransactionViewModel {
+//        val myTransactionViewModel2: TransactionViewModel by activityViewModels {
+//            TransactionViewModelFactory(
+//
+//                (requireActivity().application as TransactionApplication).database
+//                    .transactionDao()
+//            )
+//        }
+//        return myTransactionViewModel2
+//    }
+
 }
