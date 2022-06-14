b0124df75e19d7ecd8f090d75c227f9e32f46e1d
VJ1122001 <virujthakur01@gmail.com>
2022-06-14 20:26:39 +0530

    added image in recent transactions

--- app/src/main/java/com/example/trakbucks/RecentTransactionsAdapter.kt
+++ app/src/main/java/com/example/trakbucks/RecentTransactionsAdapter.kt
@@ -1,20 +1,26 @@
 package com.example.trakbucks
 
+import android.content.Context
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.navigation.findNavController
+import androidx.preference.PreferenceManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.trakbucks.data.Transaction
 import com.example.trakbucks.data.TransactionViewModel
 import com.google.android.material.card.MaterialCardView
 import com.mikhaellopez.circularimageview.CircularImageView
+import java.text.NumberFormat
+import java.util.*
 
 class RecentTransactionsAdapter() :RecyclerView.Adapter<RecentTransactionsAdapter.RecentTransactionsViewHolder>() {
 
     private lateinit var myTransactionViewModel: TransactionViewModel
     var recentTransactionList = emptyList<Transaction>()
+    lateinit var mContext : Context
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecentTransactionsViewHolder {
         val itemView = LayoutInflater.from(parent.context).inflate(R.layout.transaction,parent,false)
@@ -23,11 +29,30 @@ class RecentTransactionsAdapter() :RecyclerView.Adapter<RecentTransactionsAdapte
 
     override fun onBindViewHolder(holder: RecentTransactionsViewHolder, position: Int) {
         val currentTransaction = recentTransactionList[position]
-        //holder.profileImage_tran.setImageResource(currentTransaction.personImage)
+        holder.profileImage_tran.setImageURI(Uri.parse(currentTransaction.personImage))
         holder.name_tran.text= currentTransaction.name
-        holder.amount_tran.text=currentTransaction.amount
         holder.date_tran.text=currentTransaction.date
         holder.time_tran.text=currentTransaction.time
+
+        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences( mContext/* Activity context */)
+        val currency = sharedPreferences.getString("Currency", "INR")
+
+        when(currency)
+        {
+            "INR"->{
+                val str = NumberFormat.getCurrencyInstance(Locale("en","IN")).format(currentTransaction.amount.toLong())
+                holder.amount_tran.text=str
+            }
+            "EUR"->{
+                val str = NumberFormat.getCurrencyInstance(Locale("en","IE")).format(currentTransaction.amount.toLong())
+                holder.amount_tran.text=str
+            }
+            "DOL"->{
+                val str = NumberFormat.getCurrencyInstance(Locale("en","US")).format(currentTransaction.amount.toLong())
+                holder.amount_tran.text=str
+            }
+        }
+
 
         when(currentTransaction.type)
         {
@@ -55,4 +80,10 @@ class RecentTransactionsAdapter() :RecyclerView.Adapter<RecentTransactionsAdapte
         this.recentTransactionList = transactionList
         notifyDataSetChanged()
     }
+
+    override fun onAttachedToRecyclerView(recyclerView: RecyclerView) {
+        super.onAttachedToRecyclerView(recyclerView)
+        mContext = recyclerView.context
+    }
+
 }
