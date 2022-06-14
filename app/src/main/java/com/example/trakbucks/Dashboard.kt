53cdb1658c7f46ff244b03ef60d3aa7d31285a4d
Varin Thakur <varinthaur_co20b7_36@dtu.ac.in>
2022-06-14 14:42:02 +0530

    Add viewmodel instance.

--- app/src/main/java/com/example/trakbucks/Dashboard.kt
+++ app/src/main/java/com/example/trakbucks/Dashboard.kt
@@ -6,10 +6,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import com.example.trakbucks.data.TransactionApplication
 import com.example.trakbucks.databinding.FragmentDashboardBinding
 import com.example.trakbucks.data.TransactionViewModel
+import com.example.trakbucks.data.TransactionViewModelFactory
 
 /**
  * A simple [Fragment] subclass.
@@ -20,7 +23,13 @@ class Dashboard : Fragment() {
 
     private var _binding: FragmentDashboardBinding? = null
     private val binding get() = _binding!!
-    private lateinit var myTransactionViewModel: TransactionViewModel
+
+    private val myTransactionViewModel: TransactionViewModel by activityViewModels {
+        TransactionViewModelFactory(
+            (activity?.application as TransactionApplication).database
+                .transactionDao()
+        )
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,7 +42,6 @@ class Dashboard : Fragment() {
         // Inflate the layout for this fragment
         val fragmentBinding = DataBindingUtil.inflate<FragmentDashboardBinding>(inflater,R.layout.fragment_dashboard,container,false)
         _binding = fragmentBinding
-        myTransactionViewModel= ViewModelProvider(this).get(TransactionViewModel::class.java)
         binding.viewModel = myTransactionViewModel
         return binding.root
     }
@@ -52,12 +60,4 @@ class Dashboard : Fragment() {
     fun navigate(){
         findNavController().navigate(R.id.action_dashboard_to_profileScreen)
     }
-
-    fun setIncome(){
-
-    }
-
-    fun setExpenditure(){
-    }
-
 }
