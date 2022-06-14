fa18fb518fa3f259d6c81435a5866ece07294a5b
VJ1122001 <virujthakur01@gmail.com>
2022-06-14 20:26:55 +0530

    ordered transaction list by date and time desc

--- app/src/main/java/com/example/trakbucks/data/TransactionDao.kt
+++ app/src/main/java/com/example/trakbucks/data/TransactionDao.kt
@@ -27,7 +27,7 @@ interface TransactionDao {
     @Query("Delete from User_Details")
     suspend fun deleteUserDetails()
 
-    @Query("Select * from All_transactions_table")
+    @Query("Select * from All_transactions_table order by date desc, time desc")
     fun readAlldata(): LiveData<List<Transaction>>
 
     @Query("Select * from All_Transactions_Table order by date desc,time desc limit 3")
